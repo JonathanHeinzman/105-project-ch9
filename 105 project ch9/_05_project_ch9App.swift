33a200d5e3751c090d3d1a747f7b41fd27798246
Jonathan Heinzman <Jond.heinzman@icloud.com>
2026-06-03 18:17:37 -0600

    Initial Commit

---
+++ 105 project ch9/_05_project_ch9App.swift
@@ -0,0 +1,17 @@
+//
+//  _05_project_ch9App.swift
+//  105 project ch9
+//
+//  Created by Jonathan Heinzman on 6/3/26.
+//
+
+import SwiftUI
+
+@main
+struct _05_project_ch9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
